d4515a1e9db03e51171480aa4918fc2dd0a3c2be
edbags84 <emilybagwell@gmail.com>
2025-01-13 21:00:38 -0500

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,1 @@
 console.log("Hello World");
-
-@@ -0,0 +1,10 @@
-function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  var isMobileBreakpoint = window.innerWidth <= 641;
-  if (x.className === "navtoggle" && isMobileBreakpoint) {
-    x.className += " responsive";
-  } else {
-    x.className = "navtoggle";
-  }
-}
